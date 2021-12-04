768b2d38ba53c0519332a1f8a63163376a88fd8b
丘惟夫 <waveciou@gmail.com>
2021-12-05 00:05:58 +0800

    [case] add test case

--- src/sortBasedArray.js
+++ src/sortBasedArray.js
@@ -2,16 +2,17 @@
   * Sort array based another array's id.
   * @param { [{ id: string }] } Sorted array.
   * @param { [ string ] } Array that sorting rules.
+  * @param { string } Name of key.
   * @returns { [{ id: string }] }
   */
 
-const sortBasedArray = (dataArray, ruleArray) => {
-  const data = [...dataArray];
+const sortBasedArray = (sortedArray, ruleArray, key) => {
+  const data = [...sortedArray];
   const rule = [...ruleArray];
 
   const result = data.sort((a, b) => {
-    const A = a.id;
-    const B = b.id;
+    const A = a[key];
+    const B = b[key];
     const ruleA = rule.indexOf(A) < 0 ? rule.length : rule.indexOf(A);
     const ruleB = rule.indexOf(B) < 0 ? rule.length : rule.indexOf(B);
 
--- test/sortBasedArray.test.js
+++ test/sortBasedArray.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const sortBasedArray = require('../src/sortBasedArray');
 
-test('test', () => {
+test('basic sort', () => {
   const data = [
     { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' },
     { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' }, { id: '10' },
@@ -9,8 +9,44 @@ test('test', () => {
 
   const rule = ['3', '1', '6', '7', '8'];
 
-  expect(sortBasedArray(data, rule)).toStrictEqual([
+  expect(sortBasedArray(data, rule, 'id')).toStrictEqual([
     { id: '3' }, { id: '1' }, { id: '6' }, { id: '7' }, { id: '8' },
+    { id: '2' }, { id: '4' }, { id: '5' }, { id: '9' }, { id: '10' },
+  ]);
+});
+
+test('none rules', () => {
+  const data = [
+    { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' },
+    { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' }, { id: '10' },
+  ];
+
+  const rule = [];
+
+  expect(sortBasedArray(data, rule, 'id')).toStrictEqual([
+    { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' },
+    { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' }, { id: '10' },
+  ]);
+});
+
+test('none data', () => {
+  const data = [];
+
+  const rule = ['3', '1', '6', '7', '8'];
+
+  expect(sortBasedArray(data, rule, 'id')).toStrictEqual([]);
+});
+
+test('nonexistent rule', () => {
+  const data = [
+    { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' },
+    { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' }, { id: '10' },
+  ];
+
+  const rule = ['3', '11', '6', '7', '8'];
+
+  expect(sortBasedArray(data, rule, 'id')).toStrictEqual([
+    { id: '3' }, { id: '6' }, { id: '7' }, { id: '8' }, { id: '1' },
     { id: '2' }, { id: '4' }, { id: '5' }, { id: '9' }, { id: '10' },
   ]);
 });
